5919c900ee937bb43f761ea9cb11a8a3e22939d1
Silver <gramakov27@gmail.com>
2022-05-04 20:11:35 +0300

    футтер + правки главной страницы

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -90,6 +90,89 @@ AppAsset::register($this);
     </main>
 
     <footer>
+        <div class="footer__bg"></div>
+        <div class="footer__side-line"></div>
+        <div class="wrapper">
+            <div class="footer__banner">
+                <div class="banner__text">
+                    <div class="banner__text-main">Подпишитесь на рассылку</div>
+                    <div class="banner__text-sub">Будьте в курсе наших акций</div>
+                </div>
+
+                <div class="banner__email">
+                    <div class="banner__email-main">
+                        <input class="banner__email-input" type="email" placeholder="Ваша почта">
+                        <button class="banner__email-btn">
+                            Подписаться
+                            <svg width="44" height="10" viewBox="0 0 44 10" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path d="M43 5L43.4135 4.71884L43.6046 5L43.4135 5.28116L43 5ZM42.5865 5.28116L39.8665 1.28116L40.6935 0.718845L43.4135 4.71884L42.5865 5.28116ZM43.4135 5.28116L40.6935 9.28115L39.8665 8.71884L42.5865 4.71884L43.4135 5.28116ZM43 5.5H0.5V4.5H43V5.5Z"
+                                      fill="white"/>
+                            </svg>
+                        </button>
+                    </div>
+
+                    <div class="banner__email-info">
+                        Нажимая кнопку, вы даете согласие на обработку ваших персональных данных в соответствии с
+                        Политикой Конфиденциальности
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer__columns">
+                <div class="footer_column-contacts">
+                    <div class="column-contacts__info">
+                        <h5>Наши контакты</h5>
+                        <div>8 800 900 70 90</div>
+                        <div>г. Орск Проспект Ленина 71 / А</div>
+                        <div>Пн-вс: 10:00 - 21:00<br>без выходных</div>
+                    </div>
+
+                    <div class="column-contacts__socials">
+                        <a href="" title="Instagram">
+                            <img src="<?= Url::to(['imgs/instagram.png']) ?>" alt="instagram-logo">
+                        </a>
+                        <a href="" title="VK">
+                            <img src="<?= Url::to(['imgs/vk.png']) ?>" alt="vk-logo">
+                        </a>
+                    </div>
+
+                    <div class="column-contacts__politics">Политика конфиденциальности</div>
+                </div>
+
+                <div class="footer__divider"></div>
+
+                <div class="footer_column-sections">
+                    <h5>Разделы</h5>
+                    <div>Для одежды</div>
+                    <div>Для интерьера</div>
+                    <div>Мебельные</div>
+                    <div>Фурнитура</div>
+                    <div>Пряжа</div>
+                    <div>Новогодние</div>
+                    <div>Для шитья</div>
+                </div>
+
+                <div class="footer__divider"></div>
+
+                <div class="footer_column-payments">
+                    <div class="column-payments__info">
+                        <h5>Способы оплаты</h5>
+                        <a href="" title="VISA">
+                            <img src="<?= Url::to(['imgs/Visa.png']) ?>" alt="VISA">
+                        </a>
+                        <a href="" title="PayPal">
+                            <img src="<?= Url::to(['imgs/PayPal.png']) ?>" alt="PayPal">
+                        </a>
+                        <a href="" title="MasterCard">
+                            <img src="<?= Url::to(['imgs/MasterCard.png']) ?>" alt="MasterCard">
+                        </a>
+                    </div>
+
+                    <div class="column-payments__rights">Все права защищены (С)</div>
+                </div>
+            </div>
+        </div>
     </footer>
 
     <?php $this->endBody() ?>
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -8,37 +8,36 @@ $this->title = 'Fabrics';
 ?>
 <main>
     <section class="main__banner">
-        <div class="main__banner-outer wrapper">
-            <div class="banner__inner">
-                <h1 class="banner__inner-text">
-                    Скидки 20% на новые ткани
-                </h1>
+        <div class="main__banner-outer"></div>
+        <div class="banner__inner  wrapper">
+            <h1 class="banner__inner-text">
+                Скидки 20% на новые ткани
+            </h1>
 
-                <div class="banner__inner-controls">
-                    <div class="banner__inner-progress">
-                        <div class="banner__inner-progress_nums">
-                            2<span>/4</span>
-                        </div>
-                        <div class="banner__inner-progress_slide">
-                            <div class="banner__inner-progress_slide-current"></div>
-                        </div>
+            <div class="banner__inner-controls">
+                <div class="banner__inner-progress">
+                    <div class="banner__inner-progress_nums">
+                        2<span>/4</span>
+                    </div>
+                    <div class="banner__inner-progress_slide">
+                        <div class="banner__inner-progress_slide-current"></div>
+                    </div>
+                </div>
+
+                <div class="banner__inner-buttons">
+                    <div class="banner__inner-buttons_prev">
+                        <svg width="14" height="9" viewBox="0 0 14 9" fill="none"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1.5 4.5L5.5 0.5M1.5 4.5L5.5 8.5M1.5 4.5H14" stroke="#FBF7F4"/>
+                        </svg>
                     </div>
 
-                    <div class="banner__inner-buttons">
-                        <div class="banner__inner-buttons_prev">
-                            <svg width="14" height="9" viewBox="0 0 14 9" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <path d="M1.5 4.5L5.5 0.5M1.5 4.5L5.5 8.5M1.5 4.5H14" stroke="#FBF7F4"/>
-                            </svg>
-                        </div>
-
-                        <div class="banner__inner-buttons_next">
-                            <svg width="64" height="9" viewBox="0 0 64 9" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <path d="M63.25 4.5L63.6036 4.14645L63.9571 4.5L63.6036 4.85355L63.25 4.5ZM62.8964 4.85355L58.8964 0.853553L59.6036 0.146447L63.6036 4.14645L62.8964 4.85355ZM63.6036 4.85355L59.6036 8.85355L58.8964 8.14645L62.8964 4.14645L63.6036 4.85355ZM63.25 5H0.75V4H63.25V5Z"
-                                      fill="#BF1E77"/>
-                            </svg>
-                        </div>
+                    <div class="banner__inner-buttons_next">
+                        <svg width="64" height="9" viewBox="0 0 64 9" fill="none"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path d="M63.25 4.5L63.6036 4.14645L63.9571 4.5L63.6036 4.85355L63.25 4.5ZM62.8964 4.85355L58.8964 0.853553L59.6036 0.146447L63.6036 4.14645L62.8964 4.85355ZM63.6036 4.85355L59.6036 8.85355L58.8964 8.14645L62.8964 4.14645L63.6036 4.85355ZM63.25 5H0.75V4H63.25V5Z"
+                                  fill="#BF1E77"/>
+                        </svg>
                     </div>
                 </div>
             </div>
@@ -64,58 +63,56 @@ $this->title = 'Fabrics';
         </div>
     </section>
 
-    <section class="main__categories wrapper">
-        <div class="background__categories">
-            <div class="categories__container">
-                <div class="categories__text">
-                    <!--                    <span class="categories__text-bg">Категории</span>-->
-                    <h2>Популярные категории</h2>
-                </div>
+    <section class="main__categories">
+        <div class="categories__bg"></div>
+        <div class="categories__left-line"></div>
+        <div class="categories__container wrapper">
+            <div class="categories__text">
+                <h2>Популярные категории</h2>
+            </div>
 
-                <div class="categories__cards">
-                    <a href="">
-                        <div class="categories__cards-1">Лен и полулен</div>
-                    </a>
+            <div class="categories__cards">
+                <a href="">
+                    <div class="categories__cards-1">Лен и полулен</div>
+                </a>
 
-                    <a href="">
-                        <div class="categories__cards-2">Хлопок</div>
-                    </a>
-                    <a href="">
-                        <div class="categories__cards-3">Вискоза</div>
-                    </a>
-                    <a href="">
-                        <div class="categories__cards-4">Новогодние ткани</div>
-                    </a>
-                    <a href="">
-                        <div class="categories__cards-5">Лен и полулен</div>
-                    </a>
-                    <a href="">
-                        <div class="categories__cards-6">Хлопок</div>
-                    </a>
-                    <a href="">
-                        <div class="categories__cards-7">
-                            Вискоза
-                            <svg width="44" height="10" viewBox="0 0 44 10" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <path d="M42.5 5L39.78 1M42.5 5L39.78 9M42.5 5H-4.47035e-07" stroke="white"/>
-                            </svg>
-                        </div>
-                    </a>
-                </div>
+                <a href="">
+                    <div class="categories__cards-2">Хлопок</div>
+                </a>
+                <a href="">
+                    <div class="categories__cards-3">Вискоза</div>
+                </a>
+                <a href="">
+                    <div class="categories__cards-4">Новогодние ткани</div>
+                </a>
+                <a href="">
+                    <div class="categories__cards-5">Лен и полулен</div>
+                </a>
+                <a href="">
+                    <div class="categories__cards-6">Хлопок</div>
+                </a>
+                <a href="">
+                    <div class="categories__cards-7">
+                        Вискоза
+                        <svg width="44" height="10" viewBox="0 0 44 10" fill="none"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path d="M42.5 5L39.78 1M42.5 5L39.78 9M42.5 5H-4.47035e-07" stroke="white"/>
+                        </svg>
+                    </div>
+                </a>
             </div>
         </div>
+        <div class="categories__right-line"></div>
     </section>
 
     <section class="main__watched wrapper">
         <div class="watched__container">
             <div class="watched__text">
-                <!--                    <span class="categories__text-bg">Ранее</span>-->
                 <h2>Вы просматривали</h2>
             </div>
-
             <div class="watched__cards">
                 <div class="watched__card-1">
-                    <img src="<?= Url::to(['imgs/watched-1.png']) ?>" alt="">
+                    <a href=""><img src="<?= Url::to(['imgs/watched-1.png']) ?>" alt=""></a>
                     <p class="card__text">Рогожка</p>
                     <h4 class="card__name">Альпийская деревенька</h4>
                     <p class="card__info-1"><span>Ширина</span>1.5 м</p>
@@ -124,7 +121,7 @@ $this->title = 'Fabrics';
                     <div class="card__divider"></div>
                     <div class="card__buy">
                         <div class="card__buy_price">240 ₽<span>360 ₽</span></div>
-                        <div class="card__buy_to-basket">В корзину</div>
+                        <div class="card__buy_to-basket"><a href="">В корзину</a></div>
                     </div>
                 </div>
 
@@ -138,7 +135,7 @@ $this->title = 'Fabrics';
                     <div class="card__divider"></div>
                     <div class="card__buy">
                         <div class="card__buy_price">240 ₽<span>360 ₽</span></div>
-                        <div class="card__buy_to-basket">В корзину</div>
+                        <div class="card__buy_to-basket"><a href="">В корзину</a></div>
                     </div>
                 </div>
 
@@ -152,11 +149,10 @@ $this->title = 'Fabrics';
                     <div class="card__divider"></div>
                     <div class="card__buy">
                         <div class="card__buy_price">240 ₽<span>360 ₽</span></div>
-                        <div class="card__buy_to-basket">В корзину</div>
+                        <div class="card__buy_to-basket"><a href="">В корзину</a></div>
                     </div>
                 </div>
             </div>
     </section>
-
-    <a href="<?= Url::to(['site/catalog']) ?>">Catalog</a>
+<!--    <a href="--><?//= Url::to(['site/catalog']) ?><!--">Catalog</a>-->
 </main>
