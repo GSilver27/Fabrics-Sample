3b6dcae87447078b532e9482c46754e44d6378a7
Silver <gramakov27@gmail.com>
2022-05-08 19:28:57 +0300

    костяк каталога и фильтров, пагинация

--- frontend/views/site/catalog.php
+++ frontend/views/site/catalog.php
@@ -7,7 +7,6 @@ use frontend\assets\AppAsset;
 AppAsset::register($this);
 $this->title = 'Каталог';
 ?>
-<main>
     <section class="breadcrumbs">
         <div class="breadcrumbs-bg"></div>
         <div class="breadcrumbs-inner wrapper">
@@ -16,8 +15,8 @@ $this->title = 'Каталог';
         </div>
     </section>
 
-    <section class="catalog wrapper">
-        <div class="catalog__sidebar">
+    <main class="catalog wrapper">
+        <section class="catalog__sidebar">
             <div class="catalog__selection">
                 <h2>каталог</h2>
                 <div class="selection__cards">
@@ -48,10 +47,163 @@ $this->title = 'Каталог';
             <div class="catalog__filters">
                 <h4>Фильтры</h4>
                 <div class="catalog__divider"></div>
+                <div>
+                    Цена за метр<br>
+                    <input type="range" id="">
+                </div>
+                <div class="catalog__divider"></div>
+                <div>
+                    Ширина<br>
+                    <input type="range" id="">
+                </div>
+                <div class="catalog__divider"></div>
+                <div>
+                    Плотность<br>
+                    <input type="range" id="">
+                </div>
+                <div class="catalog__divider"></div>
             </div>
-        </div>
 
-        <div class="catalog__products">
+            <div class="catalog__types">
+                <details open>
+                    <summary>Тип ткани</summary>
+                    <input type="checkbox">Акрил
+                    <br><input type="checkbox">Альпака
+                    <br><input type="checkbox">Атлас
+                    <br><input type="checkbox">Ацетат
+                    <br><input type="checkbox">Бархат
+                    <br><input type="checkbox">Батист
+                    <br><input type="checkbox">Акрил
+                    <br><input type="checkbox">Альпака
+                    <br><input type="checkbox">Атлас
+                    <br><input type="checkbox">Ацетат
+                    <br><input type="checkbox">Бархат
+                    <br><input type="checkbox">Батист
+                </details>
+            </div>
+            <div class="catalog__divider"></div>
+
+            <div class="catalog__picture">
+                <details>
+                    <summary>Рисунок</summary>
+                    <input type="checkbox">Акрил
+                    <br><input type="checkbox">Альпака
+                    <br><input type="checkbox">Атлас
+                    <br><input type="checkbox">Ацетат
+                    <br><input type="checkbox">Бархат
+                    <br><input type="checkbox">Батист
+                    <br><input type="checkbox">Акрил
+                    <br><input type="checkbox">Альпака
+                    <br><input type="checkbox">Атлас
+                    <br><input type="checkbox">Ацетат
+                    <br><input type="checkbox">Бархат
+                    <br><input type="checkbox">Батист
+                </details>
+            </div>
+            <div class="catalog__divider"></div>
+
+            <div class="catalog__appointment">
+                <details>
+                    <summary>Назначение</summary>
+                    <input type="checkbox">Акрил
+                    <br><input type="checkbox">Альпака
+                    <br><input type="checkbox">Атлас
+                    <br><input type="checkbox">Ацетат
+                    <br><input type="checkbox">Бархат
+                    <br><input type="checkbox">Батист
+                    <br><input type="checkbox">Акрил
+                    <br><input type="checkbox">Альпака
+                    <br><input type="checkbox">Атлас
+                    <br><input type="checkbox">Ацетат
+                    <br><input type="checkbox">Бархат
+                    <br><input type="checkbox">Батист
+                </details>
+            </div>
+            <div class="catalog__divider"></div>
+
+            <div class="catalog__color">
+                <details>
+                    <summary>Цвет</summary>
+                    <input type="checkbox">Акрил
+                    <br><input type="checkbox">Альпака
+                    <br><input type="checkbox">Атлас
+                    <br><input type="checkbox">Ацетат
+                    <br><input type="checkbox">Бархат
+                    <br><input type="checkbox">Батист
+                    <br><input type="checkbox">Акрил
+                    <br><input type="checkbox">Альпака
+                    <br><input type="checkbox">Атлас
+                    <br><input type="checkbox">Ацетат
+                    <br><input type="checkbox">Бархат
+                    <br><input type="checkbox">Батист
+                </details>
+            </div>
+            <div class="catalog__divider"></div>
+
+            <div class="catalog__feature">
+                <details>
+                    <summary>Особенности</summary>
+                    <input type="checkbox">Акрил
+                    <br><input type="checkbox">Альпака
+                    <br><input type="checkbox">Атлас
+                    <br><input type="checkbox">Ацетат
+                    <br><input type="checkbox">Бархат
+                    <br><input type="checkbox">Батист
+                    <br><input type="checkbox">Акрил
+                    <br><input type="checkbox">Альпака
+                    <br><input type="checkbox">Атлас
+                    <br><input type="checkbox">Ацетат
+                    <br><input type="checkbox">Бархат
+                    <br><input type="checkbox">Батист
+                </details>
+            </div>
+            <div class="catalog__divider"></div>
+
+            <div class="catalog__compound">
+                <details>
+                    <summary>Состав</summary>
+                    <input type="checkbox">Акрил
+                    <br><input type="checkbox">Альпака
+                    <br><input type="checkbox">Атлас
+                    <br><input type="checkbox">Ацетат
+                    <br><input type="checkbox">Бархат
+                    <br><input type="checkbox">Батист
+                    <br><input type="checkbox">Акрил
+                    <br><input type="checkbox">Альпака
+                    <br><input type="checkbox">Атлас
+                    <br><input type="checkbox">Ацетат
+                    <br><input type="checkbox">Бархат
+                    <br><input type="checkbox">Батист
+                </details>
+            </div>
+            <div class="catalog__divider"></div>
+
+            <div class="catalog__manufacture">
+                <details>
+                    <summary>Cтрана производитель</summary>
+                    <input type="checkbox">Акрил
+                    <br><input type="checkbox">Альпака
+                    <br><input type="checkbox">Атлас
+                    <br><input type="checkbox">Ацетат
+                    <br><input type="checkbox">Бархат
+                    <br><input type="checkbox">Батист
+                    <br><input type="checkbox">Акрил
+                    <br><input type="checkbox">Альпака
+                    <br><input type="checkbox">Атлас
+                    <br><input type="checkbox">Ацетат
+                    <br><input type="checkbox">Бархат
+                    <br><input type="checkbox">Батист
+                </details>
+            </div>
+            <div class="catalog__divider"></div>
+
+            <div class="catalog__filters_btns">
+                <div class="btn-apply">Применить</div>
+                <div class="btn-reset">Сбросить</div>
+            </div>
+        </section>
+
+        <section class="catalog__products">
             <div class="catalog__sort">
                 <div class="catalog__sort-newest">
                     <a href="">
@@ -290,8 +442,58 @@ $this->title = 'Каталог';
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
 
-</main>
+            <section class="catalog__pagination">
+                <div class="pagination__container">
+                    <div class="pagination__prev">
+                        <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path opacity="0.3" d="M1.84264 7L8 13.0889L7.07868 14L-3.0598e-07 7L7.07868 -4.02723e-08L8 0.911083L1.84264 7Z" fill="#545155"/>
+                        </svg>
+                    </div>
 
+                    <div class="pagination__page">
+                        1
+                    </div>
+
+                    <div class="pagination__page-current">
+                        2
+                    </div>
+
+                    <div class="pagination__page">
+                        3
+                    </div>
+
+                    <div class="pagination__page">
+                        4
+                    </div>
+
+                    <div class="pagination__page">
+                        ...
+                    </div>
+
+                    <div class="pagination__page">
+                        11
+                    </div>
+
+                    <div class="pagination__next">
+                        <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6.15736 7L5.1388e-07 13.0889L0.921323 14L8 7L0.921322 -4.02723e-08L3.98247e-08 0.911083L6.15736 7Z" fill="#545155"/>
+                        </svg>
+                    </div>
+                </div>
+
+                <div class="pagination__option">
+                    <div class="pagination__option-text">Показывать<br>на странице:</div>
+                    <div class="pagination__option-amount">
+                        12
+                         <select name="" id="">
+                            <option value="empy"></option>
+                            <option value="11">13</option>
+                            <option value="11">14</option>
+                            <option value="11">15</option>
+                        </select>
+                    </div>
+                </div>
+            </section>
+        </section>
+    </main>
